60b6aa9579826172ff67c0f389d38d047c3d9b6c
YOONSUNGHYEON <qead753951@naver.com>
2023-05-30 23:03:16 +0900

    Initial commit

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,13 @@
+package hello.typeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello/v1")
+    public String helloV1(HttpServletRequest httpServletRequest) {
+
+    }
+}
